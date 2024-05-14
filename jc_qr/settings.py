4fe46b3e63fd5005bba21d813377ed17ebbfeab0
Camilo Espitia <jcespitia100@gmail.com>
2024-05-13 19:32:01 -0500

    Static build

--- jc_qr/settings.py
+++ jc_qr/settings.py
@@ -119,8 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_build', 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
